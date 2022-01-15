4e00c7b04f10bbe5c71ca5db8a43a41a506a87cd
Wesley Fisher <wesley_b_fisher@hotmail.com>
2022-01-15 16:31:45 -0400

    Animate training

--- src/animator.py
+++ src/animator.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import cv2
+import imageio
 
 class Animator:
     def __init__(self, env, episode_modulo=10):
@@ -14,6 +15,7 @@ class Animator:
         self.tau = self.period + 1.0
 
         self.images = []
+        self.name = "Animation"
     
     def start_new_episode(self):
         self.episode_count = self.episode_count + 1
@@ -23,6 +25,10 @@ class Animator:
 
     def finish(self):
         self.episode_count = 0
+
+        with imageio.get_writer(self.name+'.gif', mode='I') as writer:
+            for idx, frame in enumerate(self.images):
+                writer.append_data(frame)
     
     def step(self, state, action):
         # Don't animate all episodes
@@ -40,13 +46,15 @@ class Animator:
 
     def animate_frame(self, state, action):
         frame = self.env.env.render(mode='rgb_array')
-        frame = np.flip(frame, 2)
+        frame = np.flip(frame, 2) # OpenCV is BGR
         frame = frame.astype(np.uint8)
 
         text = f'{self.episode_count+1}'
         frame = cv2.putText(frame, text, (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
 
-        self.images.append(frame)
-
-        cv2.imshow('Name', frame)
+        cv2.imshow(self.name, frame)
         cv2.waitKey(int(self.dt*1000))
+
+        frame = np.flip(frame, 2)
+        frame = frame.astype(np.uint8)
+        self.images.append(frame)
