a1ff0ea2083eba3daaeb44459d5d976079771b94
Wesley Fisher <wesley_b_fisher@hotmail.com>
2022-01-15 16:23:59 -0400

    Render screen during episode

--- src/animator.py
+++ src/animator.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as ply
+import numpy as np
+
+import cv2
 
 class Animator:
     def __init__(self, env, episode_modulo=10):
@@ -10,6 +12,8 @@ class Animator:
         self.dt = env.dt()
         self.period = 1.0 / 30.0 # Aim for 30 fps
         self.tau = self.period + 1.0
+
+        self.images = []
     
     def start_new_episode(self):
         self.episode_count = self.episode_count + 1
@@ -18,7 +22,7 @@ class Animator:
         pass
 
     def finish(self):
-        self.episode_count = -1
+        self.episode_count = 0
     
     def step(self, state, action):
         # Don't animate all episodes
@@ -34,6 +38,15 @@ class Animator:
         
         self.tau = self.tau + self.dt
 
-    def animate_frame(self):
-        pass
+    def animate_frame(self, state, action):
+        frame = self.env.env.render(mode='rgb_array')
+        frame = np.flip(frame, 2)
+        frame = frame.astype(np.uint8)
 
+        text = f'{self.episode_count+1}'
+        frame = cv2.putText(frame, text, (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+        self.images.append(frame)
+
+        cv2.imshow('Name', frame)
+        cv2.waitKey(int(self.dt*1000))
